bc4b4c273fb52f6b2cd3b52d4539ce30c4f92df9
leif.jacky <leif.jacky@gmail.com>
2019-12-06 16:06:57 +0800

    verify with difficulty return

--- ethash.go
+++ ethash.go
@@ -125,24 +125,24 @@ type Light struct {
 }
 
 // Verify checks whether the block's nonce is valid.
-func (l *Light) Verify(block Block) bool {
+func (l *Light) VerifyWithDifficulty(block Block) (bool, *big.Int) {
 	// TODO: do ethash_quick_verify before getCache in order
 	// to prevent DOS attacks.
 	blockNum := block.NumberU64()
 	if blockNum >= epochLength*2048 {
-		log.Debug(fmt.Sprintf("block number %d too high, limit is %d", epochLength*2048))
-		return false
+		log.Debug(fmt.Sprintf("block number %d too high, limit is %d", blockNum, epochLength*2048))
+		return false, big.NewInt(0)
 	}
 
 	difficulty := block.Difficulty()
 	/* Cannot happen if block header diff is validated prior to PoW, but can
-		 happen if PoW is checked first due to parallel PoW checking.
-		 We could check the minimum valid difficulty but for SoC we avoid (duplicating)
-	   Ethereum protocol consensus rules here which are not in scope of Ethash
+	happen if PoW is checked first due to parallel PoW checking.
+	We could check the minimum valid difficulty but for SoC we avoid (duplicating)
+	Ethereum protocol consensus rules here which are not in scope of Ethash
 	*/
 	if difficulty.Cmp(common.Big0) == 0 {
 		log.Debug("invalid block difficulty")
-		return false
+		return false, big.NewInt(0)
 	}
 
 	cache := l.getCache(blockNum)
@@ -153,17 +153,22 @@ func (l *Light) Verify(block Block) bool {
 	// Recompute the hash using the cache.
 	ok, mixDigest, result := cache.compute(uint64(dagSize), block.HashNoNonce(), block.Nonce())
 	if !ok {
-		return false
+		return false, big.NewInt(0)
 	}
 
 	// avoid mixdigest malleability as it's not included in a block's "hashNononce"
 	if block.MixDigest() != mixDigest {
-		return false
+		return false, big.NewInt(0)
 	}
 
 	// The actual check.
 	target := new(big.Int).Div(maxUint256, difficulty)
-	return result.Big().Cmp(target) <= 0
+	return result.Big().Cmp(target) <= 0, result.Big()
+}
+
+func (l *Light) Verify(block Block) bool {
+	ok, _ := l.VerifyWithDifficulty(block)
+	return ok
 }
 
 func h256ToHash(in C.ethash_h256_t) common.Hash {
